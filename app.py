8d88833c1032ff14292d0686b9e0ae583f31a421
Javier558 <javierperezalejo13@gmail.com>
2025-10-06 19:36:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ BITUNIX_API_KEY = os.getenv("BITUNIX_API_KEY")
 BITUNIX_API_SECRET = os.getenv("BITUNIX_API_SECRET")
 
 # Bitunix REST endpoint
-BITUNIX_ORDER_URL = "https://openapi.bitunix.com/api/futures/v1/order"
+endpoint = "https://openapi.bitunix.com/api/v1/futures/order"
 # Optional: fixed leverage
 LEVERAGE = 50
 
